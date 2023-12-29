eb559934b607279e269b850e21bdb9e55339b03d
OmobolajiDurojaiye <durojaiyeomobolaji93@gmail.com>
2023-12-29 16:21:38 +0100

    created the edit profile form

--- pkg/forms.py
+++ pkg/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField, BooleanField, DateField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, URL
 
 
 class LoginForm(FlaskForm):
@@ -30,3 +30,13 @@ class AdminLoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class EditProfileForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    bio = TextAreaField('Bio', validators=[DataRequired()])
+    facebook = StringField('Facebook @', validators=[URL()])
+    instagram = StringField('Instagram @', validators=[URL()])
+    x = StringField('X @', validators=[URL()])
+    email = StringField('E-Mail @', validators=[Email()])
+    github = StringField('GitHub @', validators=[URL()])
--- pkg/myroutes.py
+++ pkg/myroutes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request, session
 from pkg import app
-from pkg.forms import LoginForm, RegistrationForm, BlogPostForm, AdminLoginForm
+from pkg.forms import LoginForm, RegistrationForm, BlogPostForm, AdminLoginForm, EditProfileForm
 
 #custom errors
 @app.errorhandler(404)
@@ -60,18 +60,35 @@ def index():
     
     return render_template('index.html', form=form)
 
+
 #about
 @app.route('/Who we are')
 def about():
-    return render_template("about.html")
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        userfirstname = form.userfirstname.data
+        userlastname = form.userlastname.data
+        userregemail = form.userregemail.data
+        userregpwd = form.userregpwd.data
+        userdateofbirth = form.userdateofbirth.data
+        usergender = form.usergender.data
+        agree = form.agree.data
+
+        session['username'] = userfirstname
+
+        return redirect('/login')
+    
+    return render_template('about.html', form=form)
 
 # #sign in
 # @app.route('/sign in')
 # def sign_in():
 #     return render_template('login.html')
 
+
 #blog
-@app.route('/feed')
+@app.route('/feed', methods=['GET', 'POST'])
 def feed():
     return render_template("feed.html")
 
@@ -80,17 +97,35 @@ def feed():
 def categories():
     return render_template("explore.html")
 
+
+#connect
 @app.route('/connect')
 def connect():
     return render_template("connect.html")
 
-@app.route('/profile')
+#userprofile
+@app.route('/profile', methods=['GET', 'POST'])
 def profile():
-    if session.get('useremail') == None:
+    if session.get('useremail') is None:
         return redirect('/login')
-    else:
-        return render_template("profile.html")
+    
+    form = EditProfileForm()
 
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        bio = form.bio.data
+        facebook = form.facebook.data
+        instagram = form.instagram.data
+        x = form.x.data
+        email = form.email.data
+        github = form.github.data
+
+        return render_template('profile.html', form=form)
+
+    return render_template('profile.html', form=form)
+
+#Login 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -102,12 +137,14 @@ def login():
 
     return render_template('login.html', form=form)
 
-        
+
+#Logout   
 @app.route('/logout')
 def logout():
     session.pop('useremail', None)
     return redirect('/index')
     
+#add new post
 @app.route('/newpost', methods=['GET', 'POST'])
 def create_post():
     form = BlogPostForm()
